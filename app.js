c3cc752fbf26391395fb5dd951bdac3e4deae4a4
Ven Kim <venvex@gmail.com>
2021-08-28 00:06:26 -0400

    added package json and node_modules

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs; 
+console.log(profileDataArgs);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-	if (err) throw new Error(err);
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+	if (err) throw err;
 
 	console.log('Portfolio complete! Check out index.html to see the output!');
 });
--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-const generatePage = (name, github) => {
+module.exports = (name, github) => {
 	return `
 	<!DOCTYPE html> 
 	<html lang="en"> 
@@ -16,5 +16,3 @@ const generatePage = (name, github) => {
 	</html>
 	`;
 };
-
-module.exports = generatePage;
