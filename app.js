e8361b79026f9137bc534cdcfb5f45c1c1831319
Ven Kim <venvex@gmail.com>
2021-08-28 14:03:09 -0400

    created generate-site function, revised and refactored into working status

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -134,11 +134,19 @@ Add a New Project
 
 promptUser()
 	.then(promptProject)
-	.then(portfolioData => {
-		const pageHTML = generatePage(portfolioData);
-
-		fs.writeFile('./index.html', pageHTML, err => {
-			if (err) throw new Error(err);
-			console.log('Portfolio complete! Check out index.html to see the output!');
-		});
-	});
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
